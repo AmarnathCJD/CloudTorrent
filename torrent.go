e7cf348d4154c236250607f95d92ccfe4b8d81e6
AmarnathCJD <amarnathcharichilil@gmail.com>
2022-04-15 06:22:29 +0000

    Simplify

--- torrent.go
+++ torrent.go
@@ -9,8 +9,7 @@ import (
 )
 
 var (
-	client   = InitClient()
-	Torrents = make(map[string]torrent.Torrent)
+	client = InitClient()
 )
 
 type TorrentsResponse struct {
@@ -54,58 +53,47 @@ func AddMagnet(magnet string) error {
 	if err != nil {
 		return err
 	}
-	Torrents[magnet] = *t
 	return nil
 }
 
-func GetTorrents() map[string]torrent.Torrent {
-	return Torrents
+func GetTorrents() []*torrent.Torrent {
+	return client.ListTorrents()
 }
 
-func GetTorrent(magnet string) torrent.Torrent {
-	return Torrents[magnet]
-}
-
-func GetDownloads(magnet string) {
-	x := client.ListTorrents()
-	for _, t := range x {
-		fmt.Println(t.Stats())
+func GetTorrent(id string) torrent.Torrent {
+	if Torr := client.GetTorrent(id); Torr != nil {
+		return *Torr
 	}
+	return torrent.Torrent{}
 }
 
-func CancelTorrent(magnet string) {
-	t := Torrents[magnet]
-	t.Stop()
-	delete(Torrents, magnet)
+func CancelTorrent(id string) bool {
+	Torr := client.GetTorrent(id)
+	if Torr != nil {
+		client.RemoveTorrent(id)
+		return true
+	}
+	return false
 }
 
 func GetTorrentPath(id string) string {
-	torr := client.ListTorrents()
-	for _, t := range torr {
-		if t.ID() == id {
-			return root + "/downloads/torrents/" + t.ID() + "/" + t.Stats().Name
-		}
+	if Torr := client.GetTorrent(id); Torr != nil {
+		return root + "/downloads/torrents/" + Torr.ID() + "/" + Torr.Stats().Name
 	}
 	return ""
 }
 
 func DeleteTorrentByID(id string) (bool, error) {
-	for _, t := range client.ListTorrents() {
-		if t.ID() == id {
-			err := client.RemoveTorrent(id)
-			delete(Torrents, t.Stats().InfoHash.String())
-			return true, err
-		}
+	if Torr := client.GetTorrent(id); Torr != nil {
+		err := client.RemoveTorrent(id)
+		return true, err
 	}
 	return false, nil
 }
 
-func GetTorrentStatus(magnet string) torrent.Stats {
-	torr := client.ListTorrents()
-	for _, t := range torr {
-		if t.InfoHash().String() == magnet {
-			return t.Stats()
-		}
+func GetTorrentStatus(id string) torrent.Stats {
+	if Torr := client.GetTorrent(id); Torr != nil {
+		return Torr.Stats()
 	}
 	return torrent.Stats{}
 }
@@ -113,71 +101,54 @@ func GetTorrentStatus(magnet string) torrent.Stats {
 func GetActiveTorrents() []TorrentMeta {
 	torr := client.ListTorrents()
 	Torrents := TorrentsResponse{}
-	Magnets := []string{}
 	IDno := 0
 	for _, t := range torr {
 		if t.Name() != "" {
-                        var Status = fmt.Sprint(t.Stats().Status)
-                        if Status == "Stopped" {
-Status = "Completed"
-}
-			if !StringInSlice(t.Stats().InfoHash.String(), Magnets) {
-				Magnets = append(Magnets, t.Stats().InfoHash.String())
-				IDno++
-				Torrents.Torrents = append(Torrents.Torrents, TorrentMeta{
-					Name:   t.Name(),
-					Size:   ByteCountSI(GetTorrentSize(t.ID())),
-					Perc:   GetDownloadPercentage(t.ID()),
-					Status: Status,
-					Magnet: t.Stats().InfoHash.String(),
-					Speed:  fmt.Sprint(ByteCountSI(int64(t.Stats().Speed.Download))) + "/s",
-					ID:     fmt.Sprintf("%d", IDno),
-					UID:    t.ID(),
-					Eta:    fmt.Sprint(t.Stats().ETA),
-				})
-			}
+			Torrents.Torrents = append(Torrents.Torrents, TorrentMeta{
+				Name:   t.Name(),
+				Size:   ByteCountSI(GetTorrentSize(t.ID())),
+				Perc:   GetDownloadPercentage(t.ID()),
+				Status: GetStats(t.ID()),
+				Magnet: t.Stats().InfoHash.String(),
+				Speed:  fmt.Sprint(ByteCountSI(int64(t.Stats().Speed.Download))) + "/s",
+				UID:    t.ID(),
+				Eta:    fmt.Sprint(t.Stats().ETA),
+			})
 		}
 	}
 	Torrents = SortAlpha(Torrents)
+	for i := range Torrents.Torrents {
+		Torrents.Torrents[i].ID = fmt.Sprint(IDno)
+		IDno++
+	}
 	return Torrents.Torrents
 }
 
 func GetDownloadPercentage(id string) string {
-	torr := client.ListTorrents()
-	for _, t := range torr {
-		if t.ID() == id {
-			if t.Stats().Pieces.Total != 0 {
-				p := float64(t.Stats().Pieces.Have) / float64(t.Stats().Pieces.Total)
-				return fmt.Sprintf("%.2f", p*100) + "%"
-			} else {
-				return "-"
-			}
+	torr := client.GetTorrent(id)
+	if torr != nil {
+		if torr.Stats().Pieces.Total != 0 {
+			return fmt.Sprintf("%.2f", float64(torr.Stats().Pieces.Have)/float64(torr.Stats().Pieces.Total)*100) + "%"
 		}
 	}
-	return "0" + "%"
+	return "0%"
 }
 
 func GetTorrentSize(id string) int64 {
-	torr := client.ListTorrents()
-	for _, t := range torr {
-		if t.ID() == id {
-			if t.Stats().Bytes.Total != 0 {
-				return int64(t.Stats().Bytes.Total)
-			} else {
-				return 0
-			}
+	torr := client.GetTorrent(id)
+	if torr != nil {
+		if torr.Stats().Bytes.Total != 0 {
+			return torr.Stats().Bytes.Total
 		}
 	}
 	return 0
 }
 
 func GetPeers(id string) int {
-	torr := client.ListTorrents()
-	for _, t := range torr {
-		if t.ID() == id {
-			return t.Stats().Peers.Total
-		}
-	} // soon
+	torr := client.GetTorrent(id)
+	if torr != nil {
+		return torr.Stats().Peers.Total
+	}
 	return 0
 }
 
@@ -187,8 +158,7 @@ func UpdateOnComplete() {
 
 func CheckDuplicateTorrent(magnet string) bool {
 	magnet = ParseHashFromMagnet(magnet)
-	torr := client.ListTorrents()
-	for _, t := range torr {
+	for _, t := range client.ListTorrents() {
 		if strings.ToLower(t.Stats().InfoHash.String()) == magnet {
 			return true
 		}
@@ -197,10 +167,29 @@ func CheckDuplicateTorrent(magnet string) bool {
 }
 
 func ParseHashFromMagnet(magnet string) string {
-	args := strings.Split(magnet, "&")
+	var args []string
+	if strings.Contains(magnet, "&") {
+		args = strings.Split(magnet, "&")
+	} else {
+		args = []string{magnet}
+	}
 	argv := strings.Split(args[0], "btih:")
 	if len(argv) <= 1 {
 		return ""
 	}
 	return strings.ToLower(argv[1])
+}
+
+func GetStats(id string) string {
+	torr := client.GetTorrent(id)
+	if torr != nil {
+		if torr.Stats().Bytes.Total == 0 {
+			return "Waiting"
+		} else if torr.Stats().Bytes.Downloaded == torr.Stats().Bytes.Total {
+			return "Complete"
+		} else {
+			return "Downloading"
+		}
+	}
+	return "Error"
 }
